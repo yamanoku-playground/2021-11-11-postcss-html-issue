c632ef13d8597f785405e9cb857bd16f24740080
yamanoku <0910yama@gmail.com>
2021-11-11 12:57:46 +0900

    fix: vue file only

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: ["stylelint-config-standard", "stylelint-config-recommended-vue"],
   overrides: [
     {
-      files: ["*.vue", "**/*.vue", "*.scss", "**/*.scss"],
+      files: ["*.vue", "**/*.vue"],
       customSyntax: ['postcss-html'],
       extends: ["stylelint-config-recommended-scss"],
     },
